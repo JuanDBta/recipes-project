c85c490f6d97a07280f05e007efb1f1611098693
JuanDBta <simplebetsas@gmail.com>
2023-10-02 16:45:31 -0500

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'users#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:show] do
+    resources :recipes, only: [:index, :show]
+    resources :foods, only: [:index]
+    
+    get 'public_recipes', to: 'recipes#public', on: :member
+    get 'shopping_list', to: 'foods#shopping_list', on: :member
+  end
 end
