df5cdd99d31aab490831030c60686a27d0cc34a6
JuanDBta <simplebetsas@gmail.com>
2023-12-17 21:30:12 -0500

    Added validations to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  
   has_many :food
   has_many :recipes
 end
