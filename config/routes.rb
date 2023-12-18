ecd078b3c4867ec1a63ffa28e10f025c670175a8
JuanDBta <simplebetsas@gmail.com>
2023-12-17 21:19:27 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,31 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  get 'public_recipes/index'
 
-  resources :users, only: [:show] do
-    resources :recipes, only: [:index, :show]
-    resources :foods, only: [:index]
-    
-    get 'public_recipes', to: 'recipes#public', on: :member
-    get 'shopping_list', to: 'foods#shopping_list', on: :member
-  end
+  root to: 'recipe#index', as: :user_recipes_root
+
+  get 'users/:user_id/foods', to: 'food#index', as: 'user_foods'
+  get 'users/:user_id/foods/new', to: 'food#new', as: 'new_user_food'
+  post 'users/:user_id/foods', to: 'food#create', as: 'create_user_food'
+  delete 'users/:user_id/foods/:id', to: 'food#destroy', as: 'destroy_user_food'
+
+  get 'users/:user_id/recipes', to: 'recipe#index', as: 'user_recipes'
+  get 'users/:user_id/recipes/new', to: 'recipe#new', as: 'new_user_recipe'
+  post 'users/:user_id/recipes', to: 'recipe#create', as: 'create_user_recipe'
+  get 'users/:user_id/recipes/:id', to: 'recipe#show', as: 'show_user_recipe'
+  delete 'users/:user_id/recipes/:id', to: 'recipe#destroy', as: 'destroy_user_recipe'
+
+  get 'users/:user_id/recipes/:recipe_id/recipe_foods/new', to: 'recipe_foods#new', as: 'new_user_recipe_recipe_food'
+  get 'users/:user_id/recipes/:recipe_id/recipe_foods', to: 'recipe_foods#index', as: 'user_recipe_recipe_foods'
+  post 'users/:user_id/recipes/:recipe_id/recipe_foods', to: 'recipe_foods#create', as: 'create_user_recipe_recipe_food'
+  delete 'users/:user_id/recipes/:recipe_id/recipe_foods/:id', to: 'recipe_foods#destroy', as: 'destroy_user_recipe_recipe_food'
+  get 'users/:user_id/recipes/:recipe_id/recipe_foods/:id/edit', to: 'recipe_foods#edit', as: 'edit_user_recipe_recipe_food'
+  patch 'users/:user_id/recipes/:recipe_id/recipe_foods/:id/update', to: 'recipe_foods#update', as: 'update_user_recipe_recipe_food'
+
+
+  patch 'users/:user_id/recipes/:id/toggle_public', to: 'recipe#toggle_public', as: 'toggle_public_recipe'
+
+  get '/public_recipes', to: 'public_recipes#index', as: 'public_recipes'
+
+  get '/general_shopping_list', to: 'shopping_lists#index', as: 'general_shopping_list'
+
 end
