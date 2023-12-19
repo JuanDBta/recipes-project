1b49cbc952fde421076b5109910550b4dd172937
JuanDBta <simplebetsas@gmail.com>
2023-12-18 21:47:41 -0500

    Fixed offenses

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,7 +2,7 @@ class Food < ApplicationRecord
   validates :name, :measurement_unit, :price, :quantity, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, numericality: { greater_than_or_equal_to: 0 }
-  
+
   belongs_to :user
   has_many :recipe_foods
   has_many :recipes, through: :recipe_foods
