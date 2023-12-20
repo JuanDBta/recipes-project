ea5468db8f322f23bd7374114b24a4c3a2c5c890
JuanDBta <simplebetsas@gmail.com>
2023-12-19 21:28:19 -0500

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# seeds.rb
+
+# Create some users
+user1 = User.create(name: "John Doe")
+user2 = User.create(name: "Jane Smith")
+
+# Create some foods
+food1 = Food.create(name: "Chicken Breast", measurement_unit: "grams", price: 8.99, quantity: 500, user: user1)
+food2 = Food.create(name: "Salmon Fillet", measurement_unit: "pieces", price: 12.99, quantity: 300, user: user2)
+
+# Create some recipes
+recipe1 = Recipe.create(name: "Grilled Chicken Salad", preparation_time: 20, cooking_time: 15, description: "A healthy and delicious salad with grilled chicken.", public: true, user: user1)
+recipe2 = Recipe.create(name: "Baked Salmon with Lemon", preparation_time: 15, cooking_time: 20, description: "A flavorful salmon dish baked with lemon slices.", public: false, user: user2)
+
+# Associate foods with recipes
+RecipeFood.create(quantity: 300, food: food1, recipe: recipe1)
+RecipeFood.create(quantity: 200, food: food2, recipe: recipe2)
+
+puts "Seed data created successfully."
+
